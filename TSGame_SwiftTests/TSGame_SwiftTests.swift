f857d47818b2167a803a9f083dd4667e930b803c
StoneStoneStoneWang <yuanxingfu1314@163.com>
2016-01-29 09:10:36 +0800

    Initial Commit

---
+++ TSGame_SwiftTests/TSGame_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  TSGame_SwiftTests.swift
+//  TSGame_SwiftTests
+//
+//  Created by 王磊 on 16/1/29.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+import XCTest
+@testable import TSGame_Swift
+
+class TSGame_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
